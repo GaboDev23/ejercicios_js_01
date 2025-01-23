cc05fd997c9a737ca6d00fe55dd1f159035bbdb5
Gabriel Herling <gabo.gh12398@gmail.com>
2025-01-23 15:14:44 -0300

    Generador de Pirámide de carácteres

--- Generador de Piramide/script.js
+++ Generador de Piramide/script.js
@@ -1,29 +1,44 @@
 let piramide = '';
-let contador = prompt('Introduce el tamaño de la pirámide');
-let posicion = prompt('Introduce la posición de la pirámide');
 
-if (contador === '') {
-    contador = 5;
-} else if (isNaN(contador)) {
-    contador = 5;
-}
+const altura = document.getElementById('altura');
+const caracter = document.getElementById('caracter');
+const posicion = document.getElementById('posicion');
+const generar = document.getElementById('generar');
+const resultado = document.getElementById('piramide');
 
-if (posicion === 'arriba') {
-    for (let i = contador; i >= 0; i--) {
-        for (let j = 0; j < i; j++) {
-            piramide += '*';
-        }
-        piramide += '\n';
+generar.addEventListener('click', () => {
+    let contador = altura.value;
+    let caracterElegido = caracter.value;
+
+    if (contador === '') {
+        contador = 5;
+    } else if (isNaN(contador)) {
+        contador = 5;
     }
-} else if (posicion === 'abajo') {
-    for (let i = 0; i <= contador; i++) {
-        for (let j = 0; j < i; j++) {
-            piramide += '*';
-        }
-        piramide += '\n';
-    }
-} else {
-    piramide = 'Posición no válida';
-}
 
-console.log(piramide);
+    if (caracterElegido === '') {
+        caracterElegido = '*';
+    }
+
+    if (posicion.value === '1') {
+        for (let i = 0; i <= contador; i++) {
+            for (let j = 0; j < i; j++) {
+                piramide += caracterElegido;
+            }
+            piramide += '<br>';
+        }
+    } else if (posicion.value === '2') {
+        for (let i = contador; i >= 0; i--) {
+            for (let j = 0; j < i; j++) {
+                piramide += caracterElegido;
+            }
+            piramide += '<br>';
+        }
+    } else {
+        piramide = 'Por favor, selecciona una posición válida';
+    }
+
+    resultado.style.display = 'block';
+    resultado.innerHTML = piramide;
+    piramide = '';
+});
